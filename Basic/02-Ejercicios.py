1bb1485498d1f373a866d9e5df6cb05d9765af26
ToniAlpe <242182314+ToniAlpe@users.noreply.github.com>
2026-03-01 11:33:16 +0100

    Ejercicios operadores creado

---
+++ Basic/02-Ejercicios.py
@@ -0,0 +1,35 @@
+# 1. Calcula el resto de dividir 17 entre 5 (operador módulo %) e imprímelo.
+
+
+# 2. Eleva el número 2 a la potencia de 10 (2 multiplicado por sí mismo 10 veces) e imprime el resultado.
+
+# 3. Realiza una "división entera" de 25 entre 4. 
+# El resultado debe ser 6 (sin decimales). Usa el operador //.
+
+
+# 4. Crea una variable 'comparacion' que guarde si 10 es diferente de 5. 
+# Imprime el resultado (debería ser True).
+
+
+# 5. [Lógica] Verifica si (10 > 5) Y (3 < 1) al mismo tiempo usando 'and'. 
+# Imprime el resultado.
+
+
+# 6. [Lógica] Verifica si (10 > 5) O (3 < 1) usando el operador 'or'.
+# Imprime el resultado.
+
+
+# 7. Compara dos cadenas de texto: "¿Es 'Manzana' igual a 'manzana'?" 
+# (Recuerda que Python distingue mayúsculas).
+
+
+# 8. Usa el operador 'not' para invertir el resultado de la comparacion (5 == 5).
+# Es decir, debe imprimir False aunque 5 sea igual a 5.
+
+
+# 9. Multiplica el string "Python" por el resultado de la operación (10 // 3).
+# ¿Cuántas veces aparecerá la palabra?
+
+
+# 10. Compara el largo (len) de la palabra "Hola" con el largo de la palabra "Java".
+# ¿Es el largo de "Hola" mayor o igual al de "Java"? Imprime el booleano.
